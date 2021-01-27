c0f9cab009a8bc902583e8a42ccb6d18f6c33c46
robpwatkins <robpwatkins@gmail.com>
2021-01-26 19:03:24 -0600

    time for styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { generate } from './utils/words';
@@ -6,16 +7,41 @@ import useKeyPress from './hooks/useKeyPress';
 const initialWords = generate();
 
 function App() {
-  useKeyPress(key => {
-    console.log(key);
-  });
+  const [leftPadding, setLeftPadding] = useState(
+    new Array(20).fill(' ').join(''),
+  );
+  const [outgoingChars, setOutgoingChars] = useState('');
+  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
+  const [incomingChars, setIncomingChars] = useState(initialWords.substr(1));
   
+  useKeyPress(key => {
+    let updatedOutgoingChars = outgoingChars;
+    let updatedIncomingChars = incomingChars;
+    if (key === currentChar) {
+      if (leftPadding.length > 0) {
+        setLeftPadding(leftPadding.substring(1));
+      }
+      updatedOutgoingChars += currentChar;
+      setOutgoingChars(updatedOutgoingChars);
+      setCurrentChar(incomingChars.charAt(0));
+      updatedIncomingChars = incomingChars.substring(1);
+      if (updatedIncomingChars.split(' ').length < 10) {
+        updatedIncomingChars += ' ' + generate();  
+      }
+      setIncomingChars(updatedIncomingChars);
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="Character">
+          <span className="Character-out">
+            {(leftPadding + outgoingChars).slice(-20)}
+          </span>
+          <span className="Character-current">{currentChar}</span>
+          <span>{incomingChars.substr(0, 20)}</span>
         </p>
         <a
           className="App-link"
